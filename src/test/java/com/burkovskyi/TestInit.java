3541c337d214ec0a7aa5ad02740b0c1d3f84c3bf
Man4ester <byrkovskiy@gmail.com>
2019-11-15 12:03:05 +0200

    added mock test

---
+++ src/test/java/com/burkovskyi/TestInit.java
@@ -0,0 +1,21 @@
+package com.burkovskyi;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * created by Oleksandr Burkovskyi on 2019-11-15.
+ */
+
+
+public class TestInit {
+
+
+    @Test
+    public void initTest(){
+
+        Assert.assertTrue(1 >0);
+    }
+
+}
